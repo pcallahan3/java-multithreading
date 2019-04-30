5cd0ee3b7b9830675d56d3a205a93cff0d6e96d8
Patrick <pcallahan3@mail.greenriver.edu>
2019-04-30 07:34:10 -0700

    Cleaned up MultithreadingJoinTest

--- src/multithreading/MultithreadingJoinTest.java
+++ src/multithreading/MultithreadingJoinTest.java
@@ -17,7 +17,6 @@ package multithreading;
 
 public class MultithreadingJoinTest extends Thread {
 	
-	
 	@Override
     public void run() {
         System.out.println("Thread started: "+ Thread.currentThread().getName());
@@ -30,9 +29,6 @@ public class MultithreadingJoinTest extends Thread {
     }
 
 
-	
-	
-	
 public static void main(String[] args) {
     Thread t1 = new Thread(new MultithreadingJoinTest(), "t1");
     Thread t2 = new Thread(new MultithreadingJoinTest(), "t2");
@@ -40,7 +36,6 @@ public static void main(String[] args) {
     
     t1.start();
     
-   
     try {
         t1.join(1000);
     } catch (InterruptedException e) {
@@ -50,22 +45,22 @@ public static void main(String[] args) {
     //Start 2nd thread after waiting for 1 seconds or if it's dead
     t2.start();
     
-    //start third thread only when first thread is dead
     try {
         t1.join();
     } catch (InterruptedException e) {
         e.printStackTrace();
     }
     
+    //Start third thread only when first thread is dead
     t3.start();
     
-    //let all threads finish execution before finishing main thread
+    //Let all threads finish execution before finishing main thread
     try {
         t1.join();
         t2.join();
         t3.join();
-    } catch (InterruptedException e) {
-        // TODO Auto-generated catch block
+    } 
+    catch (InterruptedException e) {
         e.printStackTrace();
     }
     
